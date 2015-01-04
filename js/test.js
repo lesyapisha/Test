ee45c8742aa9e1cb932b2ee74eedcddc6cfa72e8
lesya.pisha <lesya.pisha@gmail.com>
2015-01-04 21:37:33 +0200

    first commit

---
+++ js/test.js
@@ -0,0 +1,16 @@
+var container = document.createElement('div');
+container.className = 'container login-wrapper';
+
+document.body.appendChild(container);
+
+console.log(container);
+
+//is a function, that create new DOM element
+function createElement(tag, class, id){
+
+	var newElem = document.createElement(tag);
+
+	newElem.className = class;
+	newElem.id = 
+
+}
