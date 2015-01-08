67e4b3de17883017218aeb98f131ff2db6234b5a
lesya.pisha <lesya.pisha@gmail.com>
2015-01-09 01:23:24 +0200

    rendering of logining page

--- js/test.js
+++ js/test.js
@@ -2,20 +2,19 @@
 (function renderLoginPage(global){
 
 	var container = createElement('div', 'container login-wrapper', document.body);
-		var row = createElement('div', 'row', container);
-			var col = createElement('div', 'col-md-6 col-md-offset-3', row);
-				var h1 = createElement('h1', '', col);
-					text = document.createTextNode('Please sign in');
-					h1.appendChild(text);
-				var formHorizontal = createElement('form','form-horizontal well', col);
+	var row = createElement('div', 'row', container);
+	var col = createElement('div', 'col-md-6 col-md-offset-3', row);
+	var h1 = createElement('h1', '', col);
+		text = document.createTextNode('Please sign in');
+		h1.appendChild(text);
+	var formHorizontal = createElement('form','form-horizontal well', col);
 	
 
 	//it is a function, that create new DOM element
 	function createElement(tagName, ClassName, parent){
 		 
 		var newElem = document.createElement(tagName);
-		newElem.className = ClassName;
-		
+		newElem.className = ClassName;		
 		parent.appendChild(newElem);
 
 		return newElem;
@@ -27,37 +26,38 @@
 	function renderInputRow(inputType, text){
 
 		var formGroup = createElement('div', 'form-group', formHorizontal);
-			var labelForEmail = createElement('label','col-sm-2 control-label',formGroup);
-				textInside = document.createTextNode(text)
-				labelForEmail.appendChild(textInside);				
-			var divCol = createElement('div','col-sm-10',formGroup);
-				var input = createElement('input','form-control', divCol)
-					input.type = inputType;
+		var labelForEmail = createElement('label','col-sm-2 control-label',formGroup);
+			textInside = document.createTextNode(text)
+			labelForEmail.appendChild(textInside);				
+		var divCol = createElement('div','col-sm-10',formGroup);
+		var input = createElement('input','form-control', divCol)
+			input.type = inputType;
 	}
 
-
+	//function ask about saving user's data 
 	function renderCheckboxRow(inputType, text){
 
 		var formGroup = createElement('div', 'form-group', formHorizontal);	
-			var divCol = createElement('div','col-sm-offset-2 col-sm-10',formGroup);
-				var div = createElement('div','checkbox', divCol);
-					var label = createElement('label','', div);
-						var input = createElement('input','',label);
-							input.type = inputType;
-							textCheckbox = document.createTextNode(text);
-							label.appendChild(textCheckbox);
+		var divCol = createElement('div','col-sm-offset-2 col-sm-10',formGroup);
+		var div = createElement('div','checkbox', divCol);
+		var label = createElement('label','', div);
+		var input = createElement('input','',label);
+			input.type = inputType;
+			textCheckbox = document.createTextNode(text);
+			label.appendChild(textCheckbox);
 
 
 	}
 
+	//create button of submit
 	function renderSubmitRow(buttonType, text){
 
 		var formGroup = createElement('diu', 'form-group', formHorizontal);
-			var divCol = createElement('div', 'col-sm-offset-2 col-sm-10', formGroup);
-				var button = createElement('button', 'btn btn-primary', divCol);
-					button.type = buttonType; 
-					textInButton = document.createTextNode(text);
-					button.appendChild(textInButton);
+		var divCol = createElement('div', 'col-sm-offset-2 col-sm-10', formGroup);
+		var button = createElement('button', 'btn btn-primary', divCol);
+			button.type = buttonType; 
+			textInButton = document.createTextNode(text);
+			button.appendChild(textInButton);
 
 
 	}
